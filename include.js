1db952a7d68cc896b16e6a3e75052e40e1480147
DieCyberwerkstatt <seslija.sasha@gmail.com>
2023-05-06 23:08:19 +0200

    join added to git

---
+++ include.js
@@ -0,0 +1,19 @@
+
+/**
+ * This function get all HTML-Element, with Name '[w3-include-html]'.
+ * Fetch them and add them to HTML.
+ */
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        file = element.getAttribute("w3-include-html");
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
+}
+
